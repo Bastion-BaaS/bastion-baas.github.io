5688708d7cfe629a0a39104dfe29a7bc98f0f535
Alican Sungur <sunguralican@gmail.com>
2022-04-03 16:33:34 +0100

    build landing page

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+const words = ["servers", "custom functions", "files", "users"];
+let i = 0;
+
+const _changeText = () => {
+  i = (i + 1) % words.length;
+  document.getElementById("spin").innerHTML = words[i];
+}
+
+setInterval("_changeText()", 800);
